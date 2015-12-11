6f1555819a6edfa022652274518daad7e1239d1e
Kaka <kaka@debian>
2015-12-11 14:12:02 +0800

    test 2

--- test.php
+++ test.php
@@ -1,3 +1,19 @@
 <?php
-    echo "Hello GitHub! \n";
+/*
+$expire = 10;
+$value = array();
+
+$str = "1234567890";
+$str = substr($str, 0, -2);
+echo "str: $str \n";
+
+if ($value['set_stamp'] + $expire < 100) {                                                                            
+    echo json_encode($value)."!\n";
+    if (!empty($value)) {                                                                                                               
+        echo "1111\n";
+    } else {
+        echo "2222\n";
+    }
+}
+ */
 ?>
